4d69dbfe84d117bde47bcc8cf982763611284a5f
Guilherme Prestes <guilhermeoprestes@hotmail.com>
2018-04-19 17:01:16 -0300

    jogoforca vs01

---
+++ src/jogoforca06/JogoForca06.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogoforca06;
+
+/**
+ *
+ * @author Guilherme_Prestes
+ */
+public class JogoForca06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         telaPrincipal obj = new telaPrincipal();
+     obj.setVisible(true);
+    }
+    
+}
